046227a38470cc378c940b17a8c99c3bfaff587c
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 <stankevichuuladimiru@gmail.com>
2021-08-22 23:20:46 +0300

    feat: build mirroring

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const deg = 6;
+const hour_hand = document.querySelector('#hh');
+const min_hand = document.querySelector('#mh');
+const sec_hand = document.querySelector('#sh');
+
+setInterval (() => {
+
+let day = new Date();
+let hStep = day.getHours() * 30;;
+let mStep = day.getMinutes() * deg;;
+let sStep = day.getSeconds() * deg;;
+
+hour_hand.style.transform = `rotateZ(${hStep + (mStep/12)}deg)`;
+min_hand.style.transform = `rotateZ(${mStep}deg)`;
+sec_hand.style.transform = `rotateZ(${sStep}deg)`;
+});
